15f3a893f40092dce29ae0534877cee92c356920
Susana Solis Flores <Suzy.flores@icloud.com>
2023-11-12 13:44:56 -0500

    context-lab

--- index.js
+++ index.js
@@ -20,4 +20,114 @@ const allWagesFor = function () {
 
     return payable
 }
+function createEmployeeRecord(employeeArray) {
+    const employeeRecord = {};
+  
+    const [firstName, familyName, title, payPerHour] = employeeArray;
+  
+    employeeRecord.firstName = firstName;
+    employeeRecord.familyName = familyName;
+    employeeRecord.title = title;
+    employeeRecord.payPerHour = payPerHour;
+    employeeRecord.timeInEvents = [];
+    employeeRecord.timeOutEvents = [];
+    return employeeRecord;
+  }
+  
+  function createEmployeeRecords(employeeData) {
+    const employeeRecord = [];
+  
+    employeeData.forEach((employeeArray) => {
+      const employee = createEmployeeRecord(employeeArray); // Call the function and pass the current employeeArray
+      employeeRecord.push(employee); // Push the created employee record into the employeeRecord array
+    });
+  
+    return employeeRecord; // Return the accumulated array outside the loop
+  }
+  
+  function createTimeInEvent(dateTimeString) {
+    const [date, time] = dateTimeString.split(" ");
+    console.log(dateTimeString)
+    const [year, month, day] = date.split("-");
+    const fullDate = `${year}-${month}-${day}`;
+    const [hours, minutes] = time.match(/\d{2}/g);
+    const timeInEventObj = {
+      type: "TimeIn",
+      date: date,
+      hour: parseInt(time),
+    };
+    if (!this.timeInEvents) {
+      this.timeInEvents = [];
+    }
+    this.timeInEvents.push(timeInEventObj);
+    return this;
+  }
+
+    function createTimeOutEvent(dateTimeString) {
+      const [date, time] = dateTimeString.split(" ");
+      const [year, month, day] = date.split("-");
+      const fullDate = `${year}-${month}-${day}`;
+      const timeOutObj = {
+        type: "TimeOut",
+        date: date,
+        hour: parseInt(time),
+      };
+      if (!this.timeOutEvents) {
+        this.timeOutEvents = [];
+      }
+      this.timeOutEvents.push(timeOutObj);
+      return this;
+    }
+   
+    function hoursWorkedOnDate(dateMatched) {    
+      const timeInEvent = this.timeInEvents.find(event => event.date === dateMatched);
+      const timeOutEvent = this.timeOutEvents.find(event => event.date === dateMatched);
+      const elapsedMilliseconds = timeOutEvent.hour - timeInEvent.hour;
+      const elapsedHours = elapsedMilliseconds / 100;
+      return elapsedHours;
+    }
+    // const boundFunction = hoursWorkedOnDate(record);
+    // const dateMatched = "2022-01-01";
+    // boundFunction(dateMatched)
+  
+    function wagesEarnedOnDate(dateMatched) {
+      const [date] = dateMatched.split(" ");
+      const hoursWorked = hoursWorkedOnDate.bind(this)(dateMatched);
+      const hourlyRate = parseInt(this.payPerHour, 10);
+      const wagesEarned = hoursWorked * hourlyRate;
+      return wagesEarned;
+    }
+  
+//    function allWagesFor(record) {
+//       const dates = record.timeOutEvents.map(function(object){
+//           return object.date
+//       });
+//       let totalWages = 0;
+//       dates.forEach(function(date) {
+//           const wages = wagesEarnedOnDate.bind(this)(record, date);
+//           totalWages += wages;
+//       });
+//       return totalWages;
+//     }
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (let i = 0; i < srcArray.length; i++) {
+      if (srcArray[i].firstName === firstName) {
+        return srcArray[i];
+      }
+    }
+    return undefined;
+  }
+   
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce(function(memo, rec){
+   return memo+  allWagesFor.call(rec)
+    }, 0);
+  }
+  
+
+  
+  
+  
+      
+      
 
